d5aea634385963de323aeb6fde42a4c256013f48
Muhammad Sufyan <122455815+sufiprog@users.noreply.github.com>
2024-12-04 16:01:32 +0000

    4th

--- blog-app/src/app/page.jsx
+++ blog-app/src/app/page.jsx
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <div className="my-10 mx-20 bg-blue-100">
       <button className="my-5 mx-5 px-3 py-1 bg-white shadow-1">
-        Click Me
+        Click M
       </button>
     </div>
   );
